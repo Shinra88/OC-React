014bf9f702f7c5fa7e9c212290f355378dd356f9
Kev COLIN <colin-kevin@outlook.fr>
2023-07-03 20:22:01 +0200

    rectif scss

--- src/components/erreur/404.jsx
+++ src/components/erreur/404.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 function Error() {
 return (
-    <div className='error_contener'>
-      <h2 className='error_h2'>404</h2>
-      <p className='error_p'>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/" className='error_home'>Retourner sur la page d’accueil</Link>
+    <div className='error'>
+      <h2 className='error__h2'>404</h2>
+      <p className='error__p'>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className='error__home'>Retourner sur la page d’accueil</Link>
       </div>
   );
 }
